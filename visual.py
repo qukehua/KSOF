d1f29525b4df1abeb9f4304ccc99822b14a0d735
Kehua Qu <418221078@qq.com>
2025-06-18 10:55:20 +0800

    Update visual.py

--- visual.py
+++ visual.py
@@ -6,7 +6,7 @@ import PIL.Image as Image
 import matplotlib.animation as animation
 import imageio
 
-# h36m骨架连接顺序，每个骨架三个维度，分别为：起始关节，终止关节，左右关节标识(1 left 0 right)
+
 # human36m_connectivity_dict = [[16, 17, 0], [17, 18, 0], [18, 19, 0], [19, 21, 0], [20, 22, 0],  # 右臂
 #                               [0, 1, 0], [1, 2, 0], [2, 3, 0], [3, 4, 0], [4, 5, 0],  # 右腿
 #                               [15, 14, 0], [14, 13, 0], [13, 12, 0], [12, 11, 0],  # 躯干
@@ -54,7 +54,7 @@ def draw3Dpose(human, pose_3d, ax, lcolor="#3498db", rcolor="#e74c3c", gcolor="#
     ax.set_zlabel("z")
 
 
-# 图片背景透明化
+
 def transPNG(img):
     # img = img.convert("RGBA")
     datas = img.getdata()
@@ -68,7 +68,7 @@ def transPNG(img):
     return img
 
 
-# 图片背景由黑变白
+
 def transPNGbw(img):
     # img = img.convert("RGBA")
     datas = img.getdata()
@@ -84,16 +84,16 @@ def transPNGbw(img):
 
 def image_compose(IMAGES_PATH, IMAGE_SIZE, IMAGE_COLUMN, IMAGE_ROW, IMAGE_SAVE_PATH,
                   LEFT_DIST, RIGHT_DIST, UPPER_DIST, LOWER_DIST, OVERLOP):
-    """图像拼接函数"""
+
     if os.path.exists(IMAGE_SAVE_PATH):
         os.remove(IMAGE_SAVE_PATH)
     aft_image = Image.new('RGB',
                           (IMAGE_COLUMN * (IMAGE_SIZE[0] - LEFT_DIST - RIGHT_DIST) - OVERLOP * (IMAGE_COLUMN - 1),
-                           IMAGE_ROW * (IMAGE_SIZE[1] - UPPER_DIST - LOWER_DIST)))  # 创建一个新图
+                           IMAGE_ROW * (IMAGE_SIZE[1] - UPPER_DIST - LOWER_DIST)))  
     file_list = os.listdir(IMAGES_PATH)
-    file_list.sort(key=lambda x: int(x.split('_')[-1][:-4]))  # .png所以是[:-4]
+    file_list.sort(key=lambda x: int(x.split('_')[-1][:-4]))  
     image_names = [name for name in file_list]
-    # 循环遍历，把每张图片按顺序粘贴到对应位置上
+ 
     for y in range(1, IMAGE_ROW + 1):
         for x in range(1, IMAGE_COLUMN + 1):
             from_image = Image.open(IMAGES_PATH + image_names[IMAGE_COLUMN * (y - 1) + x - 1]).resize(
@@ -104,7 +104,7 @@ def image_compose(IMAGES_PATH, IMAGE_SIZE, IMAGE_COLUMN, IMAGE_ROW, IMAGE_SAVE_P
             aft_image.paste(from_image, ((x - 1) * (IMAGE_SIZE[0] - LEFT_DIST - RIGHT_DIST - OVERLOP),
                                          (y - 1) * (IMAGE_SIZE[1] - UPPER_DIST - LOWER_DIST)), mask=a)
             aft_image = transPNGbw(aft_image)
-    return aft_image.save(IMAGE_SAVE_PATH)  # 保存新图
+    return aft_image.save(IMAGE_SAVE_PATH) 
 
 
 def smooth(src, sample_len=20, kernel_size=0):
@@ -123,36 +123,35 @@ if __name__ == '__main__':
     path_npy = 'point_npy'
     path_img = 'img_H3.6'
     path_gif = 'gif_H3.6'
-    files = os.listdir(path_npy)  # 得到文件夹下的所有文件名称
-
-    for file in files:  # 读取文件夹下的所有文件夹（point_npy/predict_npy）
+    files = os.listdir(path_npy) 
+    for file in files:  
         # print(file)
 
-        if not os.path.isdir(file):  # 如果是文件夹才打开文件夹，并将其定义为new_path_npy
+        if not os.path.isdir(file):  
             if file == 'predict_npy':
                 human = human36m_connectivity_dict
             if file == 'truth_npy':
                 human = human36m_connectivity_truth
 
             new_path_npy = os.path.join(path_npy, file)
-            new_files = os.listdir(new_path_npy)  # 得到文件夹下的所有文件名称
+            new_files = os.listdir(new_path_npy)  
             print(new_files)
 
-            new_path_img = os.path.join(path_img, file)  # 对图像创建子目录（img_H3.6/predict_npy）
-            new_path_gif = os.path.join(path_gif, file)  # 对gif图像创建子目录（gif_H3.6/_predict_npy)
+            new_path_img = os.path.join(path_img, file)  
+            new_path_gif = os.path.join(path_gif, file)  
             if not os.path.exists(new_path_img):
                 os.makedirs(new_path_img)
             if not os.path.exists(new_path_gif):
                 os.makedirs(new_path_gif)
 
-            for new_file in new_files:  # 读取新文件夹下的所有npy（point_npy/predict_npy/smoking.npy)
-                name_path = os.path.basename(new_file)  # 分割文件名及类型 smoking.npy
-                name = os.path.splitext(name_path)[0]  # 获取文件名 smoking
-                specific_3d_skeleton = np.load(new_path_npy + '/' + name_path, allow_pickle=True)  # 打开子目录下的npy
+            for new_file in new_files: 
+                name_path = os.path.basename(new_file)  
+                name = os.path.splitext(name_path)[0] 
+                specific_3d_skeleton = np.load(new_path_npy + '/' + name_path, allow_pickle=True)  
                 # print(specific_3d_skeleton.shape)
 
-                image = os.path.join(new_path_img, name)  # 对每一个动作的png创建一个路径(img_H3.6/predict_npy/smoking)
-                gif = os.path.join(new_path_gif, name)  # 对每一个动作的gif创建一个路径(gif_H3.6/predict_npy/smoking)
+                image = os.path.join(new_path_img, name)  
+                gif = os.path.join(new_path_gif, name)  
                 if not os.path.exists(image):
                     os.makedirs(image)
                 if not os.path.exists(gif):
@@ -175,46 +174,46 @@ if __name__ == '__main__':
                         plt.ion()
                         # print(specific_3d_skeleton[i][j].shape)
                         img_path = os.path.join(image, 'batch_' + str(
-                            i))  # 对每一个batch创建一个目录 (img_H3.6/predict_npy/smoking/batch_0)
+                            i))  
                         if not os.path.exists(img_path):
                             os.makedirs(img_path)
                             # ax.lines = []
-                        ax.view_init(elev=45, azim=45)  # 角度变换，(0,20)逆x轴方向观察，(0,90)逆y轴方向观察，(90,0)逆z轴方向观察
+                        ax.view_init(elev=45, azim=45)  
                         specific_3d_skeleton[i][j] = specific_3d_skeleton[i][j][:, [0, 2, 1]]
                         draw3Dpose(human, specific_3d_skeleton[i][j], ax)
-                        plt.axis('off')  # 坐标轴关闭
-                        plt.savefig(os.path.join(img_path, name + '_' + str(j) + '.png'))  # 保存图片
-                        plt.ioff()  # 关闭画图窗口Z
+                        plt.axis('off')  
+                        plt.savefig(os.path.join(img_path, name + '_' + str(j) + '.png'))  
+                        plt.ioff()  
                         plt.close()
                         print('Saving to: ' + img_path)
 
                 for i in range(specific_3d_skeleton.shape[0]):
                     open_path = os.path.join(image,
-                                             'batch_' + str(i) + '/')  # 打开图片的地址 (img_H3.6/predict_npy/smoking/batch_0)
-                    save_path = os.path.join(gif, 'batch_' + str(i))  # 保存gif图片位置 （gif_H3.6/predict_npy/smoking/batch_0）
+                                             'batch_' + str(i) + '/')  
+                    save_path = os.path.join(gif, 'batch_' + str(i))  
                     if not os.path.exists(save_path):
                         os.makedirs(save_path)
 
-                    path = open_path  # 图片集地址
-                    size = [640, 480]  # 每张小图片的大小
-                    column = specific_3d_skeleton.shape[1]  # 列数
-                    row = 1  # 行数
+                    path = open_path 
+                    size = [640, 480]  
+                    column = specific_3d_skeleton.shape[1]  
+                    row = 1  
                     save_png = os.path.join(save_path,
-                                            name + '.png')  # 保存拼接png位置（gif_H3.6/predict_npy/smoking/batch_0/smoking.png）
+                                            name + '.png')  
                     # print(name)
                     # print(save_path)
 
-                    left_dist = 210  # 左剪裁长度
-                    right_dist = 180  # 右剪裁长度
-                    upper_dist = 90  # 上剪裁长度
-                    lower_dist = 80  # 下剪裁长度
-                    overlap = 140  # 重叠量
+                    left_dist = 210  
+                    right_dist = 180  
+                    upper_dist = 90 
+                    lower_dist = 80 
+                    overlap = 140 
                     image_compose(path, size, column, row, save_png, left_dist, right_dist, upper_dist, lower_dist,
                                   overlap)
                     print('Splicing diagram Saving to: ' + save_png)
 
                     # save_gif = os.path.join(save_path,
-                    #                         name + '.gif')  # # 保存gif动图位置（gif_H3.6/predict_npy/smoking/batch_0/smoking.gif）
+                    #                         name + '.gif') 
                     # plt.cla()
                     # files = os.listdir(open_path)
                     # files.sort(key=lambda x: int(x.split('_')[-1][:-4]))  # .png所以是[:-4]
